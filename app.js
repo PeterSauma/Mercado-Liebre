102f95b85b9347b6c907af5bdb1e2d595252e899
PeterSauma <pedrosaumacammi@gmail.com>
2022-05-18 02:40:57 -0300

    se trabajo en las hojas de formularios logIn y register

--- app.js
+++ app.js
@@ -2,30 +2,43 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const port = 3000
+const port = 3010
 
 // views
 const views = path.join(__dirname, 'views/')
 // public
 const public = path.join(__dirname, 'public/')
 // hhtp routes
-const hhtpRaiz = '/'
-const htpptHome = '/home'
+const htppRaiz = '/'
+const htppHome = '/home'
+const htppRegister = '/register'
+const httpLogin = '/login'
 // html
 const homeHtml = 'home.html'
+const registerHtml = 'register.html'
+const loginHtml = 'login.html' 
 
 // Define the static file path
 app.use(express.static(__dirname +'/public/'));
 
-app.get(hhtpRaiz, (req, res) => {
+app.get(htppRaiz, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
 })
 
-app.get(htpptHome, (req, res) => {
+app.get(htppHome, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
+})
+
+app.get(htppRegister, (req, res) => {
+    res.sendFile(path.join(views, registerHtml))
+})
+
+app.get(httpLogin, (req, res) => {
+    res.sendFile(path.join(views, loginHtml))
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
     console.log(__dirname)
 })
+
